efcdae3843dd17d920e49db04a8fdd2ba322b3eb
Feihong Hsu <feihong.hsu@gmail.com>
2018-02-20 11:22:19 -0600

    Use defstate to define service-routes
    This ensures that env will be loaded before swagger routes

--- src/clj/quickstart/routes/services.clj
+++ src/clj/quickstart/routes/services.clj
@@ -1,6 +1,6 @@
 (ns quickstart.routes.services
   (:require [ring.util.http-response :refer [ok]]
-            [compojure.api.sweet :refer [context GET defapi]]
+            [compojure.api.sweet :refer [context GET api]]
             [schema.core :as s]
             [quickstart.db.core :as db]
             [quickstart.config :refer [env]]
@@ -12,22 +12,21 @@
    :gloss String
    :pinyin String})
 
-; Seems like this shouldn't go here
-(mount/start #'quickstart.config/env)
+(mount/defstate service-routes
+  :start
+  (api
+    {:swagger {:ui "/swagger-ui"
+               :spec "/swagger.json"
+               :data {:basePath (:app-context env)
+                      :info {:version "1.0.0"
+                             :title "Sample API"
+                             :description "Sample Services"}}}}
 
-(defapi service-routes
-  {:swagger {:ui "/swagger-ui"
-             :spec "/swagger.json"
-             :data {:basePath (:app-context env)
-                    :info {:version "1.0.0"
-                           :title "Sample API"
-                           :description "Sample Services"}}}}
+    (context "/api" []
+      :tags ["api"]
 
-  (context "/api" []
-    :tags ["api"]
-
-    (GET "/random-word" []
-      :return Word
-      ; :query-params [{len :- Integer 1}]
-      :summary "Return a random word"
-      (ok (db/random-word)))))
+      (GET "/random-word" []
+        :return Word
+        ; :query-params [{len :- Integer 1}]
+        :summary "Return a random word"
+        (ok (db/random-word))))))
